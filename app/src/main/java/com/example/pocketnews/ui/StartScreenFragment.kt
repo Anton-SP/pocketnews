005de1440c4213627e4d05f9b7a0452ce6e99311
toha <toha1988cool@gmail.com>
2023-02-07 10:34:52 +0300

    add test start fragment and nav graph

---
+++ app/src/main/java/com/example/pocketnews/ui/StartScreenFragment.kt
@@ -0,0 +1,7 @@
+package com.example.pocketnews.ui
+
+import androidx.fragment.app.Fragment
+import com.example.pocketnews.R
+
+class StartScreenFragment : Fragment(R.layout.fragment_start_screen) {
+}
